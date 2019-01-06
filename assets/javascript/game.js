8cd84d5ce336fc13147c0c58d210f29a4c68e748
michelle <michelletyson29@gmail.com>
2019-01-06 10:00:27 -0800

    started pseudocode

---
+++ assets/javascript/game.js
@@ -0,0 +1,21 @@
+//create the variables
+var wins = 0,
+    losses = 0,
+    guessesRemaining = 10,
+    lettersGuessed = [];
+
+//make an array with the words the computer can choose from
+
+//key event to start the game
+
+//key events to listen for the user entered letters
+
+//compare user keys to the letters in the computer chosen word
+
+//if keys are equal, then display the correct letter
+
+//if keys are unequal, then display letter guessed, and subtract 1 from guesses remaining
+
+//continue until guesses left is zero
+
+//reset the game
